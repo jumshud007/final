0412c5df002f7415f140be0ba9a1059b35f6de12
jusmdhu1022 <zohragabulova@gmail.com>
2025-11-26 09:23:26 -0800

    first commit

---
+++ shop/script.js
@@ -0,0 +1,33 @@
+let star1 =document.getElementsByClassName("stars")[0];
+let star2 =document.getElementsByClassName("stars")[1];
+let star3 =document.getElementsByClassName("stars")[2];
+let star4 =document.getElementsByClassName("stars")[3];
+function function1(){
+    star1.textContent ="⭐⭐⭐⭐⭐"
+    star2.textContent ="⭐⭐⭐⭐⭐"
+    star3.textContent ="⭐⭐⭐⭐⭐"
+    star4.textContent ="⭐⭐⭐⭐⭐"
+}
+function function2(){
+    star1.textContent ="⭐⭐⭐⭐"
+    star2.textContent ="⭐⭐⭐⭐"
+    star3.textContent ="⭐⭐⭐⭐"
+    star4.textContent ="⭐⭐⭐⭐"
+}
+function function3(){
+    star1.textContent ="⭐⭐⭐"
+    star2.textContent ="⭐⭐⭐"
+    star3.textContent ="⭐⭐⭐"
+    star4.textContent ="⭐⭐⭐"
+}
+function function4(){
+    star1.textContent ="⭐⭐"
+    star2.textContent ="⭐⭐"
+    star3.textContent ="⭐⭐"
+    star4.textContent ="⭐⭐"
+}function function5(){
+    star1.textContent ="⭐"
+    star2.textContent ="⭐"
+    star3.textContent ="⭐"
+    star4.textContent ="⭐"
+}
